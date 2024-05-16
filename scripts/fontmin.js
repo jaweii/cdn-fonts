22f64ed9722fa9f7146990f0b83db4885e4dfc95
jawei <1005945485@qq.com>
2024-05-16 18:22:02 +0800

    feat: tiny file

---
+++ scripts/fontmin.js
@@ -0,0 +1,43 @@
+const FontMin = require('fontmin')
+const fs = require('fs')
+const rename = require('gulp-rename');
+const { get } = require('lodash')
+const tree = require('../tree.json')
+
+async function main() {
+  const txt = fs.readFileSync('./txt/words.txt', 'utf8');
+  const words = txt.split('')
+  const names = get(tree, '[0].contents[0].contents[0].contents').map(
+    (item) => item.name,
+  )
+  for (const name of names) {
+    for (const w of words) {
+      if (['', '\n'].includes(w)) continue
+      const i = words.indexOf(w)
+      const unicode = 'U+' + w.charCodeAt(0)
+      const dest = `fonts/ch/${name.replaceAll('.', '_')}/`
+      const newName = `${unicode}.${name.split('.').reverse()[0]}`
+      const exist = fs.existsSync(dest + newName)
+      if (exist) continue
+      console.log(name, `${i}/${words.length - 1}`)
+      await new Promise((resolve, reject) => {
+        const fontmin = new FontMin()
+          .src(`fonts/ch/${name}`)
+          .use(FontMin.glyph({
+            text: w,
+            hinting: false         // keep ttf hint info (fpgm, prep, cvt). default = true
+          }))
+          .use(rename(newName))
+          .dest(dest);
+        fontmin.run(function (err, files) {
+          if (err) {
+            reject(err)
+          }
+          resolve()
+        });
+      })
+    }
+  }
+}
+
+main().catch(console.error)
