d8a81bdb4aa85878033abaaaf0d88b5e9a8e9d44
jawei <1005945485@qq.com>
2024-05-17 15:10:53 +0800

    update

--- scripts/fontmin.js
+++ scripts/fontmin.js
@@ -10,6 +10,7 @@ async function main() {
   const names = get(tree, "[0].contents[0].contents").map(
     (item) => item.name
   );
+  const queue = []
   for (const name of names) {
     for (const w of words) {
       if (["", "\n"].includes(w)) continue;
@@ -20,24 +21,26 @@ async function main() {
       const exist = fs.existsSync(dest + newName);
       if (exist) continue;
       console.log(name, `${i}/${words.length - 1}`);
-      await new Promise((resolve, reject) => {
-        const fontmin = new FontMin()
-          .src(`fonts/ch/${name}`)
-          .use(
-            FontMin.glyph({
-              text: w,
-              hinting: false, // keep ttf hint info (fpgm, prep, cvt). default = true
-            })
-          )
-          .use(rename(newName))
-          .dest(dest);
-        fontmin.run(function (err, files) {
-          if (err) {
-            reject(err);
-          }
-          resolve();
-        });
+      queue.push(dest)
+      const fontmin = new FontMin()
+        .src(`fonts/ch/${name}`)
+        .use(
+          FontMin.glyph({
+            text: w,
+            hinting: false, // keep ttf hint info (fpgm, prep, cvt). default = true
+          })
+        )
+        .use(rename(newName))
+        .dest(dest);
+      fontmin.run(function (err, files) {
+        queue.splice(queue.indexOf(dest), 1)
+        if (err) {
+          console.error(err);
+        }
       });
+      while (queue.length > 20) {
+        await new Promise((resolve, reject) => setTimeout(resolve, 20))
+      }
     }
   }
 }
